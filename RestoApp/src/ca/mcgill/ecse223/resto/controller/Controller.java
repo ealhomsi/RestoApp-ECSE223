ddc4cf47e5bed91696859994b18cfd022222d34e
marievu <marie.vu@mail.mcgill.ca>
2018-04-11 16:33:03 -0400

    loyalty card changes

--- RestoApp/src/ca/mcgill/ecse223/resto/controller/Controller.java
+++ RestoApp/src/ca/mcgill/ecse223/resto/controller/Controller.java
@@ -1303,7 +1303,7 @@ public class Controller {
 	 * @param aCard
 	 * @return
 	 */
-	public static double calculatePoints(LoyaltyCard aCard) {
+	public static void calculatePoints(LoyaltyCard aCard) {
 		List<Order> allOrders = aCard.getOrders();
 		Double price = aCard.getPoint();
 
@@ -1317,7 +1317,6 @@ public class Controller {
 		aCard.setPoint(price);
 
 		RestoApplication.save();
-		return price;
 
 	}
 
